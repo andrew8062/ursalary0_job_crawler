40ea8843492c832f881f665bf8593d1ece271d9a
Andrew Chen <andrew8062@gmail.com>
2017-05-13 23:55:10 +0800

    Download ursalary job info and comment into DB
    Download ursalary job info and comment into DB

--- Database.py
+++ Database.py
@@ -11,29 +11,56 @@ class Db:
         self.conn = sqlite3.connect(self.database_name)
 
     def create_table(self):
-        self.conn.execute('''CREATE TABLE IF NOT EXISTS Job (
-        company char not null
+        self.conn.execute('''CREATE TABLE IF NOT EXISTS Jobs (
+        id INT NOT NULL PRIMARY KEY,
+        company char not null,
         title char not null,
         categoty char not null,
         location char,
-        description text ,
         salary int ,
         experience char ,
-        major char,
         education char,
-        other text
+        major char,
+        hire time,
+        description text 
         );
         ''')
         self.conn.commit()
 
-    def insert(self, title, desc, salary, experience):
-        self.conn.execute("insert into Job values (?, ?, ?, ?)", (title, desc, salary, experience))
+        self.conn.execute('''CREATE TABLE IF NOT EXISTS Comments (
+        id NOT NULL ,
+        created_time DATETIME,
+        comment text
+        );
+        ''')
+        self.conn.commit()
+
+    def insert_job(self, job_info):
+        try:
+            self.conn.execute("insert into Jobs values (?,?,?,?,?,?,?,?,?,?,?)",
+                              job_info)
+        except sqlite3.IntegrityError:
+            print("{} {} is already exist".format(job_info[0], job_info[1]))
+        except sqlite3.OperationalError:
+            print("{} cause some problem".format(job_info))
+            raise
+        self.conn.commit()
+
+    def insert_comment(self, comment):
+        try:
+            self.conn.executemany("insert into comments values (?,?,?)", comment)
+        except sqlite3.OperationalError:
+            print("{} cause some problem".format(comment))
+            raise
+
         self.conn.commit()
 
     def close(self):
         self.conn.close()
+
 if __name__ == '__main__':
     db = Db('jobs.db')
     db.create_table()
-    db.insert('engineer', 'happy', 85000, 'not bad')
+    db.insert_job(['1','普泉股份有限公司薪資', '技術員', '其它', '中部', '31000', '2 ~ 3年', '高中畢業', '', '2013-03', '\n                    \t年薪:\r\n上下班時間: \r\n年終: \r\n認股: \r\n獎金:\r\n工作內容: 負責廠內機台運作，上班12小時，輪休\r\n心得: 優點:三節獎金，年終\r\n          缺點:老闆脾氣古怪，逼走過不少的員工.....                    '])
+    db.insert_comment([['305', '2017-05-12 21:39:51', ' 看起來真恐怖，果然要這網站吔，免得人家白做事。                    ']])
     db.close()
--- Download.py
+++ Download.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 from multiprocessing.pool import Pool
 import os
 import re
+from Database import Db
 
+
+# check the last page of this page
 
 def get_last_page(url='http://www.ursalary0.com/salaries/salary_lists_tw/page:last'):
     with request.urlopen(url) as response:
@@ -12,18 +15,23 @@ def get_last_page(url='http://www.ursalary0.com/salaries/salary_lists_tw/page:la
     return int(current[0].getText())
 
 
-def get_job_links(url='http://www.ursalary0.com'):
+def get_job_links(prefix, url='http://www.ursalary0.com'):
     links=set()
     with request.urlopen(url) as response:
         soup = BeautifulSoup(response.read(), 'html.parser')
+
     for link in soup.find_all('a'):
         links.add(link.get('href'))
+
     links = list(links)
-    job_links = [x for x in links if 'salary_view_tw' in x and 'page' not in x]
+    job_links = [prefix+x for x in links if 'salary_view_tw' in x and 'page' not in x]
+
     return job_links
 
 
 def get_job_info(url):
+    job_id = os.path.basename(url)
+    print(job_id)
     with request.urlopen(url) as response:
         soup = BeautifulSoup(response.read(), 'html.parser')
 
@@ -32,34 +40,38 @@ def get_job_info(url):
 
         info = soup.findAll('div', {'class':'view_content_output'})
         info = [x.getText() for x in info]
-        comment = soup.findAll('div', {'class' : re.compile(r"^(reply_content_time pull-left|reply_content)$")})
-        comment = [x.getText() for x in comment]
-        print(comment)
         info.insert(0, title)
+        info.insert(0, job_id)
         info.append(detail)
-        print(info)
-        return info
+
+        comments = soup.findAll('div', {'class' : re.compile(r"^(reply_content_time pull-left|reply_content)$")})
+        comments = [x.getText() for x in comments]
+        # split all comments into sublist of time and comment, and append id in the front of each pair
+        comments = [ [job_id] + comments[x:x+2] for x in range(0, len(comments), 2)]
+
+        return info, comments
 
 
-def download_link(url):
-    with request.urlopen(url) as response:
-        f = open(os.path.basename(url)+'.html', 'wb')
-        f.write(response.read())
-        f.close()
+def retrieve_jobs( url):
+    db = Db("jobs.db")
+    job_info, comments = get_job_info(url)
+    db.insert_job(job_info)
+    db.insert_comment(comments)
+    db.close()
 
 
 def main():
+
+
     total_page = get_last_page()
     page_link = 'http://www.ursalary0.com/salaries/salary_lists_tw/page:'
     link_prefix = 'http://www.ursalary0.com/'
 
     for page in range(1, total_page+1):
-        job_links = get_job_links(page_link + str(page))
-        job_links = [link_prefix + x for x in job_links]
-
+        job_links = get_job_links(link_prefix, page_link + str(page))
         with Pool(4) as p:
-            p.map(get_job_info, job_links)
+            p.map(retrieve_jobs, job_links)
 
 if __name__=='__main__':
-    get_job_info('http://www.ursalary0.com/salaries/salary_view_tw/4312')
-
+    main()
+    # get_job_info('http://www.ursalary0.com/salaries/salary_view_tw/3282')
